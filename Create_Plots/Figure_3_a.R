13afd808a5a511062e65ee90f1e59116635e0bed
frgrandke <friederike.grandke@gmx.de>
2025-01-13 18:45:20 +0100

    Update Figure 3a

--- Create_Plots/Figure_3_a.R
+++ Create_Plots/Figure_3_a.R
@@ -13,6 +13,11 @@ library(ggh4x)
 
 source("scripts/helper.R")
 
+
+color_v2 <- color_v
+names(color_v2) <- labels_cell[names(color_v2)]
+color_v <- c(color_v, color_v2)
+
 degs_male <-
   read.csv(
     "results_male/de/volcano/ds_pb_limma_voom_all_col_celltype_cluster_with_additional_stats.csv",
@@ -44,7 +49,7 @@ c <-
   seq(nrow(degs_number[, colnames(degs_number) %in% c("cluster_id", "contrast", "Sex")]))
 counts <-
   aggregate(c ~ ., data = degs_number[, colnames(degs_number) %in% c("cluster_id", "contrast", "Sex")], FUN =
-              length)
+              length,drop=FALSE)
 
 counts$cluster_id[counts$cluster_id == "CD4+ Proliferating T cell"] <-
   "CD4+ Proliferating"
@@ -60,71 +65,54 @@ counts$CellAbbr <-
 counts$cluster_id <-
   factor(counts$cluster_id, levels = celltype_cluster_order)
 
-labels = ggplot(counts, aes(x = cluster_id, y = 1, fill = cluster_id)) + geom_tile() +
+labels = ggplot(counts, aes(x = CellAbbr, y = 1, fill = CellAbbr)) + geom_tile() +
   scale_fill_manual(values = color_v) +
   theme_void() + guides(fill = "none")
 
 counts$c[counts$c == 0] <- NA
 
-plot <-
-  ggplot(counts[counts$Sex == "male", ], aes(x = cluster_id, y = contrast)) +
-  geom_tile(aes(fill = c)) +
-  scale_fill_gradient2(
-    low = "white",
-    high = "#5D7322",
-    trans = "log",
-    limits = c(1, 1000),
-    breaks = c(1, 5, 50, 500)
-  ) +
-  theme_adrc() + theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1
-  )) + facet_grid(Sex ~ .) +
-  scale_x_discrete(name = "", labels = labels_cell[as.character(counts$cluster_id)]) + scale_y_discrete(name = "") +
-  labs(fill = '#DEGs ') + theme(legend.position = "bottom") + theme(legend.title =
-                                                                      element_text(size = 8),
-                                                                    legend.text = element_text(size = 6))
+plot <- ggplot(counts[counts$contrast == "ADvsHC",], aes(x = CellAbbr, y = Sex))+ 
+  geom_tile(aes(fill = c)) +  
+  scale_fill_gradient2(low = "white", high = "#ff7f00", trans = "log", limits = c(1,1000), breaks = c(1,5,50,500),na.value="white") +
+  theme_adrc() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+ facet_grid(contrast~.)+
+  scale_x_discrete(name = "", labels = labels_cell[as.character(counts$cluster_id)]) + scale_y_discrete(name = "") + 
+  labs(fill='#DEGs ') + theme(legend.position="bottom") + theme(legend.title=element_text(size=8), legend.text=element_text(size=6))
 
-plot2 <-
-  ggplot(counts[counts$Sex == "female", ], aes(x = cluster_id, y = contrast)) +
-  geom_tile(aes(fill = c)) +
-  scale_fill_gradient2(
-    low = "white",
-    high = "#9C489A",
-    trans = "log",
-    limits = c(1, 1000),
-    breaks = c(1, 5, 50, 500)
-  ) +
-  theme_adrc() + theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1
-  )) + facet_grid(Sex ~ .) +
-  scale_x_discrete(name = "", labels = labels_cell[as.character(counts$cluster_id)]) + scale_y_discrete(name = "") + labs(fill =
-                                                                                                                            '#DEGs ') +
-  theme(
-    axis.title.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank()
-  ) + theme(legend.position = "bottom") + theme(legend.title = element_text(size =
-                                                                              8),
-                                                legend.text = element_text(size = 6))
+plot2 <- ggplot(counts[counts$contrast == "MCIvsHC",], aes(x = CellAbbr, y = Sex))+ 
+  geom_tile(aes(fill = c)) +  
+  scale_fill_gradient2(low = "white", high = "#e31a1c", trans = "log", limits = c(1,1000), breaks = c(1,5,50,500),na.value="white") +
+  theme_adrc() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+ facet_grid(contrast~.)+
+  scale_x_discrete(name = "", labels = labels_cell[as.character(counts$cluster_id)]) + scale_y_discrete(name = "") + 
+  labs(fill='#DEGs ')+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) + theme(legend.position="bottom") + theme(legend.title=element_text(size=8), legend.text=element_text(size=6))
+
+plot3 <- ggplot(counts[counts$contrast == "PDvsHC",], aes(x = CellAbbr, y = Sex))+ 
+  geom_tile(aes(fill = c)) +  
+  scale_fill_gradient2(low = "white", high = "#6a3d9a", trans = "log", limits = c(1,1000), breaks = c(1,5,50,500),na.value="white") +
+  theme_adrc() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+ facet_grid(contrast~.)+
+  scale_x_discrete(name = "", labels = labels_cell[as.character(counts$cluster_id)]) + scale_y_discrete(name = "") + labs(fill='#DEGs ') +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) + theme(legend.position="bottom")+ theme(legend.title=element_text(size=8), legend.text=element_text(size=6))
+
 
 legend1 <- cowplot::get_legend(plot)
 legend2 <- cowplot::get_legend(plot2)
-plot <- plot + theme(legend.position = "none")
-plot2 <- plot2 + theme(legend.position = "none")
+legend3 <- cowplot::get_legend(plot3)
+plot <- plot + theme(legend.position="none")
+plot2 <- plot2 + theme(legend.position="none")
+plot3 <- plot3 + theme(legend.position="none")
 
-p <-
-  (plot2 %>% insert_top(labels, height = .25)) %>% insert_bottom(plot, height = 1.0)
-l <- as.ggplot(plot_grid(legend1, legend2, nrow = 1))
+
+p <- (plot3 %>% insert_top(labels, height=.25)) %>% insert_bottom(plot2, height = 1.0) %>% insert_bottom(plot, height = 1.0)
+
+
+l <- as.ggplot(plot_grid(legend1, legend2,legend3, nrow = 1))
 l <- l + theme(plot.margin = unit(c(0, 0, 1, 2), "cm"))
-figure_a <-
-  plot_grid(as.ggplot(p),
-            l,
-            nrow = 2,
-            rel_heights = c(1, 0.1))
+figure_a <- plot_grid(as.ggplot(p), l, nrow = 2, rel_heights = c(1,0.1))
+
 ggsave(
   "figures/figure_3_a.svg",
   figure_a,
